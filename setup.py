ec186bcd1dae753277c367783b631815b1783419
gabriela.lewenfus <gabriela.lewenfus@smt.ufrj.br>
2020-06-01 22:48:43 -0300

    include install_requires to setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
    author='Gabriela Lewenfus',
    author_email='gabriela.lewenfus@gmail.com',
    packages=find_packages(),
-
+   install_requires = ['scipy>=1.4.1', 'pandas>=0.15', 'scikit-learn>=0.22', 'numpy>=0.46'],
    description='Code from the paper Joint Forecasting and Interpolation of Graph Signals Using Deep Learning',
+
 )
